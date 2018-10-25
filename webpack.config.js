6d1379732e06391f3990346ebcd3b2134994a9b1
Alex <ajhollid@gmail.com>
2018-10-25 11:05:51 -0700

    Add style.css to dist

--- webpack.config.js
+++ webpack.config.js
@@ -7,9 +7,13 @@ module.exports = {
   entry: './src/scripts/mandelbrot.js',
   plugins: [
     new CleanWebpackPlugin(['dist']),
-    new CopyWebpackPlugin([{
-      from: './src/index.html', to: './index.html',
-    }]),
+    new CopyWebpackPlugin([
+      {
+        from: './src/index.html', to: './index.html',
+      }, {
+        from: './src/style.css', to: './style.css',
+      },
+    ]),
   ],
   output: {
     filename: 'bundle.js',
